29007529891072e04a27d0bfcc9b03a0d2b586ab
Sriram1920 <112625430+Sriram1920@users.noreply.github.com>
2022-09-07 12:55:27 +0530

    Add files via upload

---
+++ Sriram 4.py
@@ -0,0 +1,7 @@
+name1 = input('enter the user name: ')
+name2 = input('re enter the user name: ')
+if (name1 == name2):
+    print('the user name is valid')
+    
+else:
+    print('the user name is invalid')
